42a3c71f797a7baaad638e3e0355e9092c68dd5c
vabarbosa <va@us.ibm.com>
2016-08-10 16:05:44 -0400

    multiple grouped bar charts

--- vis/simpledatavis-groupedbarchart.js
+++ vis/simpledatavis-groupedbarchart.js
@@ -49,117 +49,154 @@
         });
       }
 
+      // separate data into multiple series to be charted
+      var series = [];
+      var featuredGroups = [];
+      var featured = (typeof opts.featured === 'string') ? [opts.featured] : (opts.featured || []);
+      var maxGroups = opts.maxgroups && !isNaN(parseInt(opts.maxgroups,10)) ? Number(opts.maxgroups) : data.length;
+      if (maxGroups) {
+        var row = [];
+        for (var i = 0; i < data.length; i++) {
+          if (featured.indexOf(data[i].key) != -1) {
+            featuredGroups.push(data[i]);
+          }
+          else {
+            row.push(data[i]);
+            if (row.length == maxGroups) {
+              series.push(row);
+              row = [];
+            }
+          }
+        }
+        if (row.length > 0) {
+          series.push(row);
+        }
+      }
+      if (featuredGroups.length > 0) {
+        series.unshift(featuredGroups);
+      }
 
-      // var wpb = data.length <= 10 ? 60 : data.length <= 20 ? 40 : 20;
-      // var width = Math.max(400, (wpb * data.length));
-
-      var margin = {top: 20, right: 150, bottom: 100, left: 80};
-      var box = selection.node().getBoundingClientRect();
-      var width = Math.max(800, box.width) - margin.left - margin.right;
-      var height = 500 - margin.top - margin.bottom;
+      data = series;
 
       var color = d3.scale.category10();
-
-      var xScale = d3.scale.ordinal().rangeRoundBands([0, width], .25);
-      var groupScale = d3.scale.ordinal();
-      var yScale = d3.scale.linear().range([height, 0]);
-
-      // scale the data
-      xScale.domain(data.map(function(d) { return d.key; }));
-      groupScale.domain(groupKeys).rangeRoundBands([0, xScale.rangeBand()]);
-      yScale.domain([0, d3.max(data, function(d) { return d3.max(d.groups, function(d) { return d.value; }); })]);
-
-      // define the axis
-      var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
-      var yAxis = d3.svg.axis().scale(yScale).orient('left').tickFormat(d3.format('.2s'));
+      var margin = {top: 20, right: 150, bottom: 120, left: 80};
+      var box = selection.node().getBoundingClientRect();
+      var width = Math.max(800, box.width) - margin.left - margin.right;
+      var height = (data.length > 1 ? 450 : 600) - margin.top - margin.bottom;
 
       // setup the svg element
       var svg = selection.selectAll('svg').data([data]);
       svg.enter().append('svg');
       svg.attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom);
+        .attr('height', (height + margin.top + margin.bottom) * data.length);
+      svg.exit().remove();
 
-      var graph = svg.selectAll('g').data([data]);
-      graph.enter().append('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+      // setup series graph for each series
+      var sGraph = svg.selectAll('g.series').data(data);
+      sGraph.enter().append('g')
+          .attr('class', 'series')
+      sGraph.attr('transform', function(d, i) {
+        return 'translate(' + margin.left + ',' + (margin.top + (height + margin.bottom) * i) + ')'
+      })
+      sGraph.exit().remove();
 
-      // setup the x axis
-      var xaxis = graph.selectAll('g.x').data([data]);
-      xaxis.enter().append('g')
-        .attr('class', 'x axis')
-        .style('font-size', '0.8rem');
-      xaxis.transition()
-        .attr('transform', 'translate(0,' + height + ')')
-        .attr('opacity', 1)
-        .call(xAxis)
-        .selectAll('text')
-          .attr('y', 7)
-          .attr('x', 7)
-          .attr('dy', '.35em')
-          .attr('transform', 'rotate(45)')
-          .style('text-anchor', 'start');
-      xaxis.exit().remove();
+      sGraph.each(function(sData, index) {
+        var graph = d3.select(this);
 
-      // setup the y axis
-      var yaxis = graph.selectAll('g.y').data([data]);
-      yaxis.enter().append('g')
-        .attr('class', 'y axis')
-        .style('font-size', '0.8rem');
-      yaxis.transition()
-        .attr('opacity', 1)
-        .call(yAxis);
-      yaxis.exit().remove();
+        var nb = sData.length * groupKeys.length;
+        var bw = nb <= 15 ? 55 : sData.length <= 21 ? 35 : 15;
 
-      // style the axis
-      graph.selectAll('.axis path')
-        .style('fill', 'none')
-        .style('stroke', '#152935');
-      graph.selectAll('.axis line')
-        .style('fill', 'none')
-        .style('stroke', '#152935');
-      xaxis.selectAll('path')
-        .style('stroke', 'none')
+        var xScale = d3.scale.ordinal().rangeRoundBands([0, nb * bw], .25);
+        var groupScale = d3.scale.ordinal();
+        var yScale = d3.scale.linear().range([height, 0]);
 
-      var group = graph.selectAll('.group').data(data);
-      // add new groups
-      group.enter().append('g')
-        .attr('class', 'group')
-        .attr('opacity', 0);
-      // update groups
-      group.transition()
-        .attr('opacity', 1)
-        .attr('transform', function(d) { return 'translate(' + xScale(d.key) + ',0)'; });
-      // remove old groups
-      group.exit().transition()
-        .attr('opacity', 0)
-        .remove();
+        // define the axis
+        var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
+        var yAxis = d3.svg.axis().scale(yScale).orient('left').tickFormat(d3.format('.2s'));
 
-      var bars = group.selectAll('rect.bar').data(function(d) { return d.groups; });
-      // add new bars
-      bars.enter().append('rect')
-        .attr('opacity', 0);
-      // update bars
-      bars.transition()
-        .attr('class', function(d, i) { return 'bar bar-'+d.key.replace(/\s+/g, ''); })
-        .attr('opacity', 1)
-        .attr('width', groupScale.rangeBand())
-        .attr('x', function(d) { return groupScale(d.key); })
-        .attr('y', function(d) { return d.value ? yScale(d.value) : height; })
-        .attr('height', function(d) { return d.value ? height - yScale(d.value) : 0; })
-        .style('fill', function(d) { return color(d.key); })
-        .each('end', function(d) {
-          d3.select(this)
-            .on('mouseover', function(d, i) {
-              SimpleDataVis.tooltip.mouseover(d, i, opts);
-            })
-            .on('mousemove', SimpleDataVis.tooltip.mousemove)
-            .on('mouseout', SimpleDataVis.tooltip.mouseout);
-        });
-      // remove old bars
-      bars.exit().transition()
-        .attr('opacity', 0)
-        .attr('width', 0)
-        .remove();
+        // scale the data
+        xScale.domain(sData.map(function(d) { return d.key; }));
+        groupScale.domain(groupKeys).rangeRoundBands([0, xScale.rangeBand()]);
+        yScale.domain([0, d3.max(sData, function(d) { return d3.max(d.groups, function(d) { return d.value; }); })]);
+
+        // setup the x axis
+        var xaxis = graph.selectAll('g.x').data([sData]);
+        xaxis.enter().append('g')
+          .attr('class', 'x axis')
+          .style('font-size', '0.8rem')
+        xaxis.transition()
+          .attr('transform', 'translate(0,' + height + ')')
+          .attr('opacity', 1)
+          .call(xAxis)
+          .selectAll('text')
+            .attr('y', 7)
+            .attr('x', 7)
+            .attr('dy', '.35em')
+            .attr('transform', 'rotate(45)')
+            .style('text-anchor', 'start');
+        xaxis.exit().remove();
+
+        // setup the y axis
+        var yaxis = graph.selectAll('g.y').data([sData]);
+        yaxis.enter().append('g')
+          .attr('class', 'y axis')
+          .style('font-size', '0.8rem')
+        yaxis.transition()
+          .attr('opacity', 1)
+          .call(yAxis);
+        yaxis.exit().remove();
+
+        // style the axis
+        graph.selectAll('.axis path')
+          .style('fill', 'none')
+          .style('stroke', '#152935');
+        graph.selectAll('.axis line')
+          .style('fill', 'none')
+          .style('stroke', '#152935');
+        xaxis.selectAll('path')
+          .style('stroke', 'none')
+
+        var group = graph.selectAll('.group').data(sData);
+        // add new groups
+        group.enter().append('g')
+          .attr('class', 'group')
+          .attr('opacity', 0);
+        // update groups
+        group.transition()
+          .attr('opacity', 1)
+          .attr('transform', function(d) { return 'translate(' + xScale(d.key) + ',0)'; });
+        // remove old groups
+        group.exit().transition()
+          .attr('opacity', 0)
+          .remove();
+
+        var bars = group.selectAll('rect.bar').data(function(d) { return d.groups; });
+        // add new bars
+        bars.enter().append('rect')
+          .attr('opacity', 0);
+        // update bars
+        bars.transition()
+          .attr('class', function(d, i) { return 'bar bar-'+d.key.replace(/\s+/g, ''); })
+          .attr('opacity', 1)
+          .attr('width', groupScale.rangeBand())
+          .attr('x', function(d) { return groupScale(d.key); })
+          .attr('y', function(d) { return d.value ? yScale(d.value) : height; })
+          .attr('height', function(d) { return d.value ? height - yScale(d.value) : 0; })
+          .style('fill', function(d) { return color(d.key); })
+          .each('end', function(d) {
+            d3.select(this)
+              .on('mouseover', function(d, i) {
+                SimpleDataVis.tooltip.mouseover(d, i, opts);
+              })
+              .on('mousemove', SimpleDataVis.tooltip.mousemove)
+              .on('mouseout', SimpleDataVis.tooltip.mouseout);
+          });
+        // remove old bars
+        bars.exit().transition()
+          .attr('opacity', 0)
+          .attr('width', 0)
+          .remove();
+      });
 
       // legend key
       var legendkey = svg.selectAll('rect.legend').data(groupKeys);
